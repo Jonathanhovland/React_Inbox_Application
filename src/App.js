57b6599523d6ac3aa153820d0922dcb9dc0df2e7
jonathanhovland <jonathan.hovland@yahoo.com>
2018-11-14 10:55:28 -0700

    MessageList and single Message components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Toolbar from './Components/Toolbar';
+import MessageList from './Components/MessageList';
 
 class App extends Component {
   render() {
     return (
       <div classNameName="App">
         <Toolbar />
+        <MessageList />
       </div>
     );
   }
---
+++ src/Components/MessageList.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Message from "./Message";
+
+const MessageList = () => {
+    return (
+        <Message />
+    )
+}
+
+export default MessageList
